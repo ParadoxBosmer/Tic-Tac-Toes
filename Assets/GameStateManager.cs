acb98f7f6e60af6942a513375724028af26909d3
Dusan-Stanimirovic <dussanstanimirovic@gmail.com>
2026-04-27 12:31:47 +0200

    Added Draw state and turn counter

--- Assets/GameStateManager.cs
+++ Assets/GameStateManager.cs
@@ -11,10 +11,16 @@ public class GameStateManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI winnerText;
     [SerializeField] private TextMeshProUGUI finalTimeText;
     [SerializeField] private GameObject endPopup;
+    [SerializeField] private TextMeshProUGUI crossesCount;
+    [SerializeField] private TextMeshProUGUI circlesCount;
     public static GameStateManager Instance { get; private set; }
 
     private GameStates state;
     private bool paused ;
+    private int max_turns = 9;
+    private int current_turns;
+    private int circles;
+    private int crosses;
     int[] player_1_win_con = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
     int[] player_2_win_con = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
 
@@ -33,34 +39,62 @@ public class GameStateManager : MonoBehaviour
     {
         state = GameStates.Player1Turn;
         paused = false;
+        current_turns = 0;
+        circles = 4;
+        crosses = 5;
+        crossesCount.text = crosses.ToString();
+        circlesCount.text = circles.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateText()
     {
-
+        switch (state)
+        {
+            case GameStates.Player1Turn:
+            {
+                crosses--;
+                crossesCount.text = crosses.ToString();
+                break;
+            }
+            case GameStates.Player2Turn:
+            {
+                circles--;
+                circlesCount.text = circles.ToString();
+                break;
+            }
+        }
     }
-
     public void GetNextTurn()
     {
         switch (state)
         {
-
             case GameStates.Player1Turn:
-                //if not wincon
+            {
                 state = GameStates.Player2Turn;
-                break;
-            //else
-            //state= Game.States.Player1Wins;
+                current_turns++;
+                if (current_turns == max_turns)
+                {
+                    state = GameStates.Draw;
+                    winnerText.text = "Draw";
+                    float time = GetComponentInParent<Timer>().GetTime();
+                    float minutes = Mathf.FloorToInt(time / 60);
+                    float seconds = Mathf.FloorToInt(time % 60);
+                    finalTimeText.text =
+                        string.Format("The round laster {0:00} minutes and {1:00} seconds", minutes, seconds);
 
-            case GameStates.Player2Turn:
-                //if not wincon
-                state = GameStates.Player1Turn;
-                break;
-            //else
-            //state= Game.States.Player2Wins;
+                    endPopup.SetActive(true);
+                }
 
+                break;
         }
+            case GameStates.Player2Turn:
+            {
+                state = GameStates.Player1Turn;
+                current_turns++;
+                break;
+            }
+        }
+        
     }
 
     public GameStates GetCurrentTurn()
@@ -83,51 +117,61 @@ public class GameStateManager : MonoBehaviour
     public void updateScore(int row, int col)
     {
         if (paused) return;
-        
-        if (state == GameStates.Player1Turn)
+
+        switch (state)
         {
-            player_1_win_con[row]++;
-            player_1_win_con[col + 3]++;
-            if (row == col)
-                player_1_win_con[6]++;
-            if (2 - row == col)
-                player_1_win_con[7]++;
-
-            if (player_1_win_con[row] == 3 || player_1_win_con[col + 3] == 3 || player_1_win_con[6] == 3 ||
-                player_1_win_con[7] == 3)
+            case GameStates.Player1Turn:
             {
+                UpdateText();
+                player_1_win_con[row]++;
+                player_1_win_con[col + 3]++;
+                if (row == col)
+                    player_1_win_con[6]++;
+                if (2 - row == col)
+                    player_1_win_con[7]++;
 
-                winnerText.text = "PLayer 1 won";
-                float time = GetComponentInParent<Timer>().GetTime();
-                float minutes = Mathf.FloorToInt(time / 60);
-                float seconds = Mathf.FloorToInt(time % 60);
-                finalTimeText.text =
-                    string.Format("The round laster {0:00} minutes and {1:00} seconds", minutes, seconds);
+                if (player_1_win_con[row] == 3 || player_1_win_con[col + 3] == 3 || player_1_win_con[6] == 3 ||
+                    player_1_win_con[7] == 3)
+                {
 
-                endPopup.SetActive(true);
-                state = GameStates.Player1Win;
+                    winnerText.text = "PLayer 1 won";
+                    float time = GetComponentInParent<Timer>().GetTime();
+                    float minutes = Mathf.FloorToInt(time / 60);
+                    float seconds = Mathf.FloorToInt(time % 60);
+                    finalTimeText.text =
+                        string.Format("The round laster {0:00} minutes and {1:00} seconds", minutes, seconds);
+
+                    endPopup.SetActive(true);
+                    state = GameStates.Player1Win;
+                    current_turns++;
+                }
+
+                break;
             }
-
-        }
-        else if (state == GameStates.Player2Turn)
-        {
-            player_2_win_con[row]++;
-            player_2_win_con[col + 3]++;
-            if (row == col)
-                player_2_win_con[6]++;
-            if (2 - row == col)
-                player_2_win_con[7]++;
-
-            if (player_2_win_con[row] == 3 || player_2_win_con[col + 3] == 3 || player_2_win_con[6] == 3 ||
-                player_2_win_con[7] == 3)
+            case GameStates.Player2Turn:
             {
-                endPopup.SetActive(true);
-                float time = GetComponentInParent<Timer>().GetTime();
-                float minutes = Mathf.FloorToInt(time / 60);
-                float seconds = Mathf.FloorToInt(time % 60);
-                finalTimeText.text = string.Format("The round laster {0,0:0} minutes and {1:0:0} seconds", minutes,
-                    seconds);
-                state = GameStates.Player2Win;
+                UpdateText();
+                player_2_win_con[row]++;
+                player_2_win_con[col + 3]++;
+                if (row == col)
+                    player_2_win_con[6]++;
+                if (2 - row == col)
+                    player_2_win_con[7]++;
+
+                if (player_2_win_con[row] == 3 || player_2_win_con[col + 3] == 3 || player_2_win_con[6] == 3 ||
+                    player_2_win_con[7] == 3)
+                {
+                    endPopup.SetActive(true);
+                    float time = GetComponentInParent<Timer>().GetTime();
+                    float minutes = Mathf.FloorToInt(time / 60);
+                    float seconds = Mathf.FloorToInt(time % 60);
+                    finalTimeText.text = string.Format("The round laster {0:00} minutes and {1:00} seconds", minutes,
+                        seconds);
+                    state = GameStates.Player2Win;
+                    current_turns++;
+                }
+
+                break;
             }
         }
     }
@@ -137,12 +181,18 @@ public class GameStateManager : MonoBehaviour
         player_1_win_con = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
         player_2_win_con = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
         state = GameStates.Player1Turn;
+        current_turns = 0;
 
         var tiles = GameObject.FindGameObjectsWithTag("Tile");
         foreach (var tile in tiles)
         {
             var sprite = tile.GetComponent<SpriteRenderer>().sprite = null;
         }
+
+        crosses = 5;
+        circles = 4;
+        crossesCount.text = crosses.ToString();
+        circlesCount.text = circles.ToString();
 
         GetComponentInParent<Timer>().RestartTimer();
     }
